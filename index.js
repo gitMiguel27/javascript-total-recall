7086b4fdf52b8349cc245dd83ca0a44b8664617b
Miguel Nazario <95545880+gitMiguel27@users.noreply.github.com>
2024-11-14 11:56:39 -0500

    finished part 2 part a

--- index.js
+++ index.js
@@ -90,3 +90,25 @@ PART A: Q + A
         console.log("Sorry, you're too young.");
     };
 
+// PART 2: LOOPS
+
+// PART A: THE BASICS
+
+// Write a loop that will print out all the numbers from 0 to 10, inclusive
+    for (let i = 0; i <= 10; i++) {
+        console.log(i);
+    };
+// Write a loop that will print out all the numbers from 10 up to and including 400
+    for (let i = 10; i <= 400; i++) {
+        console.log(i);
+    };
+// Write a loop that will print out every third number starting with 12 and going no higher than 4000
+    for (let i = 12; i < 4000; i += 3) {
+        console.log(i);
+    };
+
+// PART B: GET EVEN
+
+// Print out the numbers that are within the range of 1 - 100
+    
+// Adjust your code to add a message next to even numbers only that says: "<-- is an even number"
