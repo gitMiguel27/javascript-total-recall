37487ecfabd36a75e7646ab30647bef6ae0173b9
Miguel Nazario <95545880+gitMiguel27@users.noreply.github.com>
2024-11-14 11:39:18 -0500

    finished part c

--- index.js
+++ index.js
@@ -40,3 +40,26 @@ PART A: Q + A
 // Create a variable called yourName and set it equal to your name as a string. Then, write an expression that takes the string "Hello, my name is " and the variable yourName so that it returns a new string with them concatenated.
     let yourName = "Miguel";
     console.log("Hello, my name is " + yourName);
+
+// PART C: BOOLEANS
+
+// Using the provided variable definitions, replace the blanks so that all log statements print true in the console. Answers should be all be valid JS syntax and not weird things that don't make sense but happen to print true to the console
+    const a = 4; const b = 53; const c = 57; const d = 16; const e = 'Kevin';
+
+    console.log(a < b); 
+    console.log(c > d); 
+    console.log('Name' == 'Name'); 
+    
+    // FOR THE NEXT TWO, USE ONLY && OR || 
+    console.log(true || false);
+    console.log(false && false && false && false && false || true);
+
+    console.log(false === false);
+    console.log(e == 'Kevin'); 
+    console.log(a + b >= c); 
+    // note: a < b < c is NOT CORRECT (and is not a valid JS expression, think about using other math operations)
+
+    console.log(a * a <= d); 
+    // note: the answer is a simple arithmetic equation, not something "weird" 
+    
+    console.log(48 == '48');
