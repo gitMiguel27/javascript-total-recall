6e4789dbd4103e02a51730c14167c9ea4e5ec392
Miguel Nazario <95545880+gitMiguel27@users.noreply.github.com>
2024-11-14 11:24:39 -0500

    finished part a

---
+++ index.js
@@ -0,0 +1,27 @@
+/* 
+PART 1: VARIABLES AND DATA TYPES
+
+PART A: Q + A
+
+1. How do we assign a value to a variable?
+    - With the assignment operator. 
+
+2. How do we change the value of a variable? 
+    - By reassigning a new value.
+
+3. How do we assign an existing variable to a new variable?
+    - Declaring the new vairable and assigning its value to the existing variable.
+
+4. Remind me, what are declare, assign, and define?
+    - Declaring a vairable means to name a new variable. You can assign a variable its value with the assignment operator. Defining is the process of declaring and assigning.
+
+5. What is pseudocoding and why should you do it?
+    - Pseudocoding is when you write down comments on your thought process/ approach to tackling a problem. This helps you organize your thoughts and creates a game plan for what you need to accomplish step-by-step.
+
+6. What percentage of time should be spent thinking about how you're going to solve a problem vs actually typing in code to solve it?
+    - I would say around 35% should be given to thinking of game plan. However, it is important to think of a game plan before you start coding so that you avoid going back and forth with your code. Coding should then take up the rest of your effort because you can also start working from something foundational up to something fully developed.
+
+*/
+
+// PART B: STRINGS
+
